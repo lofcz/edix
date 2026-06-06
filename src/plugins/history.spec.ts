4c4e4dd0692080bffebe7ce94fcf2218321822c0
inokawa <48897392+inokawa@users.noreply.github.com>
2026-06-06 14:03:40 +0900

    Add tests for history plugin (#346)

--- src/plugins/history.spec.ts
+++ src/plugins/history.spec.ts
@@ -137,7 +137,82 @@ it("undo delete linebreak", () => {
   expect(editor.selection).toEqual(updatedSelection);
 });
 
-it("undo set attr", () => {
+it("undo replace text", () => {
+  const doc: DocNode = {
+    children: [
+      { children: [{ text: "abcde" }] },
+      { children: [{ text: "fghij" }] },
+    ],
+  };
+  const selection: Selection = [1, 2];
+  const editor = createEditor({ doc });
+  editor.selection = selection;
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  const text = "xyz";
+  editor
+    .apply({ type: "delete", range: selection })
+    .apply({ type: "insert_text", at: selection[0], text });
+  const updatedDoc: DocNode = {
+    children: [
+      { children: [{ text: "axyzcde" }] },
+      { children: [{ text: "fghij" }] },
+    ],
+  };
+  const updatedSelection: Selection = [
+    selection[0] + text.length,
+    selection[0] + text.length,
+  ];
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(updatedSelection);
+
+  editor.exec(Undo);
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  editor.exec(Redo);
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(updatedSelection);
+});
+
+it("undo replace linebreak", () => {
+  const doc: DocNode = {
+    children: [
+      { children: [{ text: "abcde" }] },
+      { children: [{ text: "fghij" }] },
+    ],
+  };
+  const selection: Selection = [5, 6];
+  const editor = createEditor({ doc });
+  editor.selection = selection;
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  const text = "xyz";
+  editor
+    .apply({ type: "delete", range: selection })
+    .apply({ type: "insert_text", at: selection[0], text });
+  const updatedDoc: DocNode = {
+    children: [{ children: [{ text: "abcdexyzfghij" }] }],
+  };
+  const updatedSelection: Selection = [
+    selection[0] + text.length,
+    selection[0] + text.length,
+  ];
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(updatedSelection);
+
+  editor.exec(Undo);
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  editor.exec(Redo);
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(updatedSelection);
+});
+
+it("undo format text", () => {
   const doc: DocNode = {
     children: [
       { children: [{ text: "abcde" }] },
@@ -160,6 +235,43 @@ it("undo set attr", () => {
     children: [
       { children: [{ text: "a" }, { text: "b", foo: "bar" }, { text: "cde" }] },
       { children: [{ text: "fghij" }] },
+    ],
+  };
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(selection);
+
+  editor.exec(Undo);
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  editor.exec(Redo);
+  expect(editor.doc).toEqual(updatedDoc);
+  expect(editor.selection).toEqual(selection);
+});
+
+it("undo format lines", () => {
+  const doc: DocNode = {
+    children: [
+      { children: [{ text: "abcde" }] },
+      { children: [{ text: "fghij" }] },
+    ],
+  };
+  const selection: Selection = [1, 7];
+  const editor = createEditor({ doc });
+  editor.selection = selection;
+  expect(editor.doc).toEqual(doc);
+  expect(editor.selection).toEqual(selection);
+
+  editor.apply({
+    type: "set_attr",
+    range: selection,
+    key: "foo",
+    value: "bar",
+  });
+  const updatedDoc: DocNode = {
+    children: [
+      { children: [{ text: "a" }, { text: "bcde", foo: "bar" }] },
+      { children: [{ text: "f", foo: "bar" }, { text: "ghij" }] },
     ],
   };
   expect(editor.doc).toEqual(updatedDoc);
@@ -210,5 +322,3 @@ it("undo set node attr", () => {
   expect(editor.doc).toEqual(updatedDoc);
   expect(editor.selection).toEqual(selection);
 });
-
-// TODO test batched undo
