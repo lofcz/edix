91cd5b5e2d637ba3d48b9f2df36bd03812c498aa
inokawa <48897392+inokawa@users.noreply.github.com>
2026-06-23 21:44:34 +0900

    Update utils.spec.ts

--- src/doc/utils.spec.ts
+++ src/doc/utils.spec.ts
@@ -1,9 +1,43 @@
 import { describe, expect, it } from "vitest";
-import { stringToFragment } from "./utils.js";
-import { type Fragment } from "./types.js";
+import { sliceFragment, stringToFragment } from "./utils.js";
+import { type DocNode, type Fragment, type Range } from "./types.js";
+import { getNodeSize } from "./node.js";
+
+describe(sliceFragment.name, () => {
+  const doc: DocNode = {
+    children: [
+      { children: [{ text: "Hello" }] },
+      { children: [{ text: "world" }] },
+    ],
+  };
+  const size0 = getNodeSize(doc.children[0]!);
+  const size1 = getNodeSize(doc.children[1]!);
+  it.each<[Range, Fragment]>([
+    [[1, 0], []],
+    [[0, 0], []],
+    [[0, size0], [{ children: [{ text: "Hello" }] }]],
+    [
+      [0, size0 + 1],
+      [{ children: [{ text: "Hello" }] }, { children: [{ text: "" }] }],
+    ],
+    [
+      [0, size0 + 2],
+      [{ children: [{ text: "Hello" }] }, { children: [{ text: "w" }] }],
+    ],
+    [
+      [size0, size0 + 1],
+      [{ children: [{ text: "" }] }, { children: [{ text: "" }] }],
+    ],
+    [[size0 + 1, size0 + size1], [{ children: [{ text: "worl" }] }]],
+    [[0, size0 + size1 + 1], doc.children],
+    [[0, Infinity], doc.children],
+  ])(`$0`, (range, res) => {
+    expect(sliceFragment(doc, ...range)).toEqual(res);
+  });
+});
 
 describe(stringToFragment.name, () => {
-  const tests: [string, Fragment][] = [
+  it.each<[string, Fragment]>([
     ["Hello world", [{ children: [{ text: "Hello world" }] }]],
     [
       "Hello\n world",
@@ -22,11 +56,7 @@ describe(stringToFragment.name, () => {
         { children: [{ text: "" }] },
       ],
     ],
-  ];
-
-  tests.forEach(([str, doc]) => {
-    it(str, () => {
-      expect(stringToFragment(str)).toEqual(doc);
-    });
+  ])(`$0`, (str, doc) => {
+    expect(stringToFragment(str)).toEqual(doc);
   });
 });
